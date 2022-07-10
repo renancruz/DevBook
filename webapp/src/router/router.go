1515765d217866743fccc146c700965d5caa137e
Renan Cruz <renan.frcr@gmail.com>
2022-07-09 21:57:52 -0300

    criando e configurando a rota de login

--- webapp/src/router/router.go
+++ webapp/src/router/router.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"webapp/src/router/rotas"
+
+	"github.com/gorilla/mux"
+)
 
 // Gerar retornar  um router com todas a rotas configuradas
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
