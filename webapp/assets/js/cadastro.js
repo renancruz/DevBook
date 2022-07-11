2ca0c148f52d17ee330874fc77d0db2e02e753a1
Renan Cruz <renan.frcr@gmail.com>
2022-07-11 15:13:24 -0300

    criando o pacote de respostas do webapp

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -17,5 +17,12 @@ function criarUsuario(evento) {
       nick: $("#nick").val(),
       senha: $("#senha").val(),
     },
-  });
+  })
+    .done(function () {
+      alert("Usuário cadastrado com sucesso!");
+    })
+    .fail(function (erro) {
+      console.log(erro);
+      alert("Erro ao cadastrar o usuário");
+    });
 }
