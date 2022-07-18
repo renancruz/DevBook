913167bac527db8f7a90afbdcc8530bb2f5fb5f8
Renan Cruz <renan.frcr@gmail.com>
2022-07-18 17:05:16 -0300

    add a funcionalidade de criar uma publicação

--- webapp/go.mod
+++ webapp/go.mod
@@ -2,9 +2,8 @@ module webapp
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
-
 require (
-	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/securecookie v1.1.1
+	github.com/joho/godotenv v1.4.0
 )
--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -9,9 +9,9 @@ import (
 
 // Rota representa todas as rotas da Aplicação web
 type Rota struct {
-	URI string
-	Metodo string
-	Funcao func(http.ResponseWriter, *http.Request)
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
 }
 
@@ -20,17 +20,18 @@ func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
 	rotas = append(rotas, rotasUsuarios...)
 	rotas = append(rotas, rotaPaginaPrincipal)
+	rotas = append(rotas, rotasPublicacoes...)
 
 	for _, rota := range rotas {
 
 		if rota.RequerAutenticacao {
-			router.HandleFunc(rota.URI, 
+			router.HandleFunc(rota.URI,
 				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
-			).Methods(rota.Metodo)	
+			).Methods(rota.Metodo)
 		} else {
-			router.HandleFunc(rota.URI, 
+			router.HandleFunc(rota.URI,
 				middlewares.Logger(rota.Funcao),
-			).Methods(rota.Metodo)	
+			).Methods(rota.Metodo)
 		}
 
 	}
