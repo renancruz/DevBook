1cdf3e1cafd60767cbafc28b92dd67f67fc8c866
Renan Cruz <renan.frcr@gmail.com>
2022-07-19 16:02:55 -0300

    curtir publicacao

--- webapp/src/respostas/respostas.go
+++ webapp/src/respostas/respostas.go
@@ -12,13 +12,16 @@ type ErroAPI struct {
 }
 
 // JSON retorna uma respotas em formato JSON para a requisição
-func JSON(w http.ResponseWriter, statusCode int, dados interface{}){
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.Header().Set("content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
+
 }
 
 // TratarStatusCodeDeErro trata as requisições com status code 400 ou superior
